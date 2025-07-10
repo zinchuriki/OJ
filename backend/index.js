41419ed0db0afaa9b5693149dc27ed374bf197c8
zinchuriki <priyanshu999.ps@gmail.com>
2025-07-10 15:18:02 +0530

    again some changes.

--- backend/index.js
+++ backend/index.js
@@ -1,31 +1,48 @@
 const express = require("express");
 const app = express();
 const { DBConnection } = require("./database/db");
-const User = require("./models/User")
+const User = require("./models/User");
+const bcrypt = require("bcryptjs");
 
 DBConnection();
 app.get("/", (req, res) => {
   res.send("hello,world!");
 });
 
-app.post("/register", async(req, res) => {
-  const { firstname, lastname, email, password } = req.body;
+app.post("/register", async (req, res) => {
+ try {
+   const { firstname, lastname, email, password } = req.body;
 
   if (!(firstname && lastname && email && password)) {
     return res.status(400).send("Please enter all the details");
   }
 
-const existingUser=await User.findOne({email});
+  const existingUser = await User.findOne({ email });
 
-if(existingUser){
-  return res.status(400).send("User already exists with the same email")
-}
+  if (existingUser) {
+    return res.status(400).send("User already exists with the same email");
+  }
 
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const user = await User.create({
+    firstname,
+    lastname,
+    email,
+    password: hashedPassword,
+  });
 
+  const token=JsonWebTokenError.sign({id: user._id,email},process.env.SECRET_KEY,{
+    expireIn: '1h',
+  })
 
-
+  user.token=token;
+  user.password=undefined;
+  res.status(200).json({ message: 'you have successfully registered',user});
+ } catch (error) {
+    console.log(error);
+ }
 });
 
 app.listen(5000, () => {
-  console.log("Server is listening on port 5000");
+  console.log(`Server is listening on port ${process.env.PORT}!`);
 });

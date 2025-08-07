675b078075137eaa66e8ac794b9b85bb9d3cd701
zinchuriki <priyanshu999.ps@gmail.com>
2025-08-07 16:53:36 +0530

    some formatting.

--- backend/generateFile.js
+++ backend/generateFile.js
@@ -1,21 +1,21 @@
-const fs = require('fs');
-const path = require('path');
-const { v4: uuid } = require('uuid');
+const fs = require("fs");
+const path = require("path");
+const { v4: uuid } = require("uuid");
 
-const dirCodes = path.join(__dirname, 'codes');
+const dirCodes = path.join(__dirname, "codes");
 
 if (!fs.existsSync(dirCodes)) {
-    fs.mkdirSync(dirCodes, { recursive: true });
+  fs.mkdirSync(dirCodes, { recursive: true });
 }
 
 const generateFile = async (format, content) => {
-    const jobID = uuid();
-    const filename = `${jobID}.${format}`;
-    const filePath = path.join(dirCodes, filename);
-    await fs.writeFileSync(filePath, content);
-    return filePath;
+  const jobID = uuid();
+  const filename = `${jobID}.${format}`;
+  const filePath = path.join(dirCodes, filename);
+  fs.writeFileSync(filePath, content);
+  return filePath;
 };
 
 module.exports = {
-    generateFile,
+  generateFile,
 };
